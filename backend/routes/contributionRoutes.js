6c428522fbb49913901cf82b0c2e3e207656412c
Pt. Gangadhar Vidyadhar Mayadhar Onkarnath Shashtri <LALILAL0@Oms-MacBook-Pro-5.local>
2026-04-08 22:25:29 +0530

    contributer dashboard added

--- backend/routes/contributionRoutes.js
+++ backend/routes/contributionRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Working");
-});
+const {
+  getMyFighters,
+  getPending,
+  getApproved,
+  getRejected,
+} = require("../controllers/contributorController");
+
+const { protect, authorize } = require("../middleware/authMiddleware");
+
+router.get("/my", protect, authorize("contributor"), getMyFighters);
+router.get("/pending", protect, authorize("contributor"), getPending);
+router.get("/approved", protect, authorize("contributor"), getApproved);
+router.get("/rejected", protect, authorize("contributor"), getRejected);
 
 module.exports = router;
